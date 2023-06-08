e063a27e5774b0fccf98a74e86f178bd8575330b
tajkir-alam <tajkir.alam@gmail.com>
2023-06-08 20:24:26 +0600

    classes section title added

--- src/Components/SectionTitle.jsx
+++ src/Components/SectionTitle.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SectionTitle = ({ subHeading, heading }) => {
     return (
-        <div className='mt-14 mb-8 mx-8 lg:w-1/3 lg:mx-auto text-center space-y-4'>
-            <h2 className='text-[#D99904] text-xl italic tracking-wider'>---{subHeading}---</h2>
-            <h1 className='text-4xl border-y-4 py-4 uppercase'>{heading}</h1>
+        <div className='mt-14 mb-5 mx-8 lg:w-1/3 lg:mx-auto text-center space-y-2 p-4 rounded-lg font-Courgette'>
+            <h1 className='text-4xl uppercase'>{heading}</h1>
+            <h2 className='text-[#d79c62] text-xl italic tracking-wider'>{subHeading}</h2>
         </div>
     );
 };
--- src/Pages/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/PopularClasses/PopularClasses.jsx
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import Classes from './Classes';
+import SectionTitle from '../../../Components/SectionTitle';
 
 const PopularClasses = () => {
     const [classes, setClasses] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:5000/class?limit=6&sort=-1')
-        .then(res => res.json())
-        .then(data => setClasses(data))
+            .then(res => res.json())
+            .then(data => setClasses(data))
     }, [])
 
     return (
-        <section className='custom-container grid gap-5 md:grid-cols-2 lg:grid-cols-3'>
-            {
-                classes.map(classes => <Classes key={classes._id} classes={classes}></Classes>)
-            }
-        </section>
+        <>
+            <SectionTitle
+                heading={'Top Classes'}
+                subHeading={'Meet the top classes you can choose. Start your summer vacation with us.'}
+            />
+
+            <section className='custom-container grid gap-5 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    classes.map(classes => <Classes key={classes._id} classes={classes}></Classes>)
+                }
+            </section>
+        </>
     );
 };
 
