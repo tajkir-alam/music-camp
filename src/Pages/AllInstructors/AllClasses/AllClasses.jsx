ef3b9a517c46f1996193a11092ff43c2978d9434
tajkir-alam <tajkir.alam@gmail.com>
2023-06-10 00:31:32 +0600

    btn disabled based on available seats condition

--- src/Pages/AllInstructors/AllClasses/AllClasses.jsx
+++ src/Pages/AllInstructors/AllClasses/AllClasses.jsx
@@ -14,10 +14,10 @@ const AllClasses = () => {
             return res.json();
         }
     })
-    
+
     return (
         <div className='custom-container grid lg:grid-cols-3 gap-4 my-12'>
-            { loader && useLoader()}
+            {loader && useLoader()}
             {
                 classes.map(singClass =>
                     <div className="card card-compact bg-base-200 duration-500 shadow-xl dark:hover:shadow-white dark:hover:shadow-md scale-100 hover:scale-105" key={singClass._id}>
@@ -30,7 +30,11 @@ const AllClasses = () => {
                             <p className='font-semibold text-slate-600 dark:text-slate-500'>Class will be Taken by <span className='font-bold dark:text-slate-400'>{singClass.instructorName}</span></p>
                             <div className="card-actions justify-end items mt-4">
                                 <p className='text-lg mt-2 font-medium'>Available Seats: {singClass.availableSeats}</p>
-                                <button className="btn text-white tracking-wider bg-slate-900 hover:bg-slate-700 dark:bg-slate-700 dark:hover:bg-slate-900">Enroll Class</button>
+                                <button className="btn text-white tracking-wider bg-slate-900 hover:bg-slate-700 dark:bg-slate-700 dark:hover:bg-slate-900"
+                                    disabled={singClass.availableSeats === 0 ? true : false}
+                                >
+                                    Enroll Class
+                                </button>
                             </div>
                         </div>
                     </div>
