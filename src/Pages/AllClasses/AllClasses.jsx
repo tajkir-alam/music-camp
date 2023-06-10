3e04f617ed9d021f690d6d941870439de2fe2ecc
tajkir-alam <tajkir.alam@gmail.com>
2023-06-11 01:44:46 +0600

    cart btn disbale conditionally the class is already added or not

--- src/Pages/AllClasses/AllClasses.jsx
+++ src/Pages/AllClasses/AllClasses.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
@@ -18,7 +18,7 @@ const AllClasses = () => {
     const [axiosSecure] = useAxiosSecure();
 
     const { data: classes = [] } = useQuery({
-        queryKey: ['instructors'],
+        queryKey: ['classes'],
         queryFn: async () => {
             setLoader(true);
             const res = await fetch('http://localhost:5000/classes');
@@ -26,7 +26,21 @@ const AllClasses = () => {
             return res.json();
         }
     })
-    
+
+    const { data: cartAdded = [], refetch } = useQuery({
+        queryKey: ['cartAdded'],
+        queryFn: async () => {
+            // setLoader(true);
+            const res = await axiosSecure.get('/added-to-cart');
+            // setLoader(false);
+            return res.data;
+        }
+    })
+
+    // Card Btn will be disabled if anyone added any class to his cart or if any class found in his class.
+    const classIds = classes.map(singClass => singClass._id);
+    const filteredCartAdded = cartAdded.filter(item => classIds.includes(item.classId));
+
     const addToCartBtn = (id) => {
         if (!user) {
             Swal.fire({
@@ -46,11 +60,11 @@ const AllClasses = () => {
         else if (user && isStudent) {
             const addClass = classes.find(classIs => classIs._id === id);
             const { image, name, instructorEmail, instructorName, price, availableSeats, _id } = addClass;
-            const cartClass = { image, name, instructorEmail, instructorName, price, availableSeats, classId: _id };
+            const cartClass = { userEmail: user.email, image, name, instructorEmail, instructorName, price, availableSeats, classId: _id };
             axiosSecure.post('/cart', cartClass)
                 .then(data => {
                     if (data.data.insertedId) {
-                        // reset(); 
+                        refetch();
                         Swal.fire({
                             position: 'top-end',
                             icon: 'success',
@@ -67,29 +81,32 @@ const AllClasses = () => {
         <div className='custom-container grid lg:grid-cols-3 gap-4 my-12'>
             {loader && useLoader()}
             {
-                classes.map(singClass =>
-                    <div className={singClass.availableSeats === 0 ? 'bg-[#FF0000] card card-compact duration-500 shadow-xl dark:hover:shadow-white dark:hover:shadow-md scale-100 hover:scale-105' :
-                        `card card-compact bg-base-100 duration-500 shadow-xl dark:hover:shadow-white dark:hover:shadow-md scale-100 hover:scale-105`}
-                        key={singClass._id}>
-                        <figure><img src={singClass.image} alt="" className='h-52 w-full' /></figure>
-                        <div className="card-body">
-                            <div className="flex items-center ite">
-                                <h2 className="card-title text-2xl dark:text-slate-300">{singClass.name}</h2>
-                                <p className='text-right font-medium text-xl text-slate-700 dark:text-slate-400 mr-2'>${singClass.price}</p>
-                            </div>
-                            <p className='font-semibold text-slate-600 dark:text-slate-500'>Class will be Taken by <span className='font-bold dark:text-slate-400'>{singClass.instructorName}</span></p>
-                            <div className="card-actions justify-end items mt-4">
-                                <p className='text-lg mt-2 font-medium'>Available Seats: {singClass.availableSeats}</p>
-                                <button onClick={() => addToCartBtn(singClass._id)}
-                                    className="btn text-white tracking-wider btn-error dark:bg-slate-700 dark:hover:bg-slate-900"
-                                    disabled={singClass.availableSeats === 0 ? true : false || isAdmin && true || isInstructor && true}
-                                >
-                                    Add To Cart
-                                </button>
+                classes.map(singClass => {
+                    const isAddedToCart = filteredCartAdded.some(item => item.classId === singClass._id);
+                    return (
+                        <div className={singClass.availableSeats === 0 ? 'bg-[#FF0000] card card-compact duration-500 shadow-xl dark:hover:shadow-white dark:hover:shadow-md scale-100 hover:scale-105' :
+                            `card card-compact bg-base-100 duration-500 shadow-xl dark:hover:shadow-white dark:hover:shadow-md scale-100 hover:scale-105`}
+                            key={singClass._id}>
+                            <figure><img src={singClass.image} alt="" className='h-52 w-full' /></figure>
+                            <div className="card-body">
+                                <div className="flex items-center ite">
+                                    <h2 className="card-title text-2xl dark:text-slate-300">{singClass.name}</h2>
+                                    <p className='text-right font-medium text-xl text-slate-700 dark:text-slate-400 mr-2'>${singClass.price}</p>
+                                </div>
+                                <p className='font-semibold text-slate-600 dark:text-slate-500'>Class will be Taken by <span className='font-bold dark:text-slate-400'>{singClass.instructorName}</span></p>
+                                <div className="card-actions justify-end items mt-4">
+                                    <p className='text-lg mt-2 font-medium'>Available Seats: {singClass.availableSeats}</p>
+                                    <button onClick={() => addToCartBtn(singClass._id)}
+                                        className="btn text-white tracking-wider btn-error dark:bg-slate-700 dark:hover:bg-slate-900"
+                                        disabled={singClass.availableSeats === 0 || isAdmin || isInstructor || isAddedToCart}
+                                    >
+                                        Add To Cart
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
+                    )
+                })
             }
         </div>
     );
--- src/Pages/Dashboard/StudentDashboard/StudentCart.jsx
+++ src/Pages/Dashboard/StudentDashboard/StudentCart.jsx
@@ -1,6 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const StudentCart = () => {
+    const { data: classes = [] } = useQuery({
+        queryKey: ['instructors'],
+        queryFn: async () => {
+            setLoader(true);
+            const res = await fetch('http://localhost:5000/classes');
+            setLoader(false);
+            return res.json();
+        }
+    })
     return (
         <div>
             this is the student cart
