dc5359b7bb7ef1124ec56f4aa9f30fba0c47904f
tajkir-alam <tajkir.alam@gmail.com>
2023-06-12 03:36:32 +0600

    adding class done

--- src/Pages/Dashboard/InstructorDashboard/AddClass.jsx
+++ src/Pages/Dashboard/InstructorDashboard/AddClass.jsx
@@ -2,15 +2,45 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
 
 const AddClass = () => {
     const { user } = useAuth();
     const navigate = useNavigate();
+    const [axiosSecure] = useAxiosSecure();
 
     const { register, handleSubmit, watch, reset, formState: { errors } } = useForm();
     const onSubmit = data => {
-        console.log(data);
+        data.availableSeats = parseInt(data.availableSeats);
+        data.price = parseInt(data.price);
 
+        const course = {
+            image: data.image,
+            students: 0,
+            name: data.name,
+            instructorImg: user.photoURL,
+            instructorName: user.displayName,
+            instructorEmail: user.email,
+            availableSeats: data.availableSeats,
+            price: data.price,
+            status: "Pending"
+        }
+
+        axiosSecure.post('/course', course)
+            .then(data => {
+                if (data.data.insertedId) {
+                    console.log(data.data);
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Your item has been added',
+                        showConfirmButton: false,
+                        timer: 1000
+                    })
+                    // navigate('/');
+                }
+            })
     };
 
     return (
@@ -21,56 +51,41 @@ const AddClass = () => {
                     <div className="space-y-5">
                         <div className="grid lg:grid-cols-2 items-center gap-3">
                             <div className="space-y-1">
-                                <label htmlFor="picture" className='block font-medium dark:text-[#ffff] ml-1'>Enter a valid image URL</label>
-                                <input {...register("picture")} required placeholder='Enter URL here @example: https://img.com' className='py-2 px-3 shadow-lg border-2 outline-none rounded-md w-full' />
+                                <label htmlFor="image" className='block font-medium dark:text-[#ffff] ml-1'>Enter Class Thumbnail image URL</label>
+                                <input {...register("image", { required: true })} placeholder='Enter URL here @example: https://img.com' className='py-2 px-3 shadow-lg dark:bg-slate-700 border-2 outline-none rounded-md w-full' />
+                                {errors.image && <span>This field is required</span>}
                             </div>
                             <div className="space-y-1">
-                                <label htmlFor="name" className='block font-medium ml-1 dark:text-[#ffff]'>Enter the product name</label>
-                                <input {...register("name")} required id="" placeholder='Enter the name here' className='py-2 px-3 shadow-lg border-2 outline-none rounded-md w-full' />
+                                <label htmlFor="name" className='block font-medium ml-1 dark:text-[#ffff]'>Enter the Class name</label>
+                                <input {...register("name", { required: true })} id="" placeholder='Enter the name here' className='py-2 px-3 shadow-lg dark:bg-slate-700 border-2 outline-none rounded-md w-full' />
+                                {errors.name && <span>This field is required</span>}
                             </div>
                         </div>
                         <div className="grid lg:grid-cols-2 gap-3 items-center">
                             <div className='space-y-1'>
-                                <label htmlFor="sellerName" className='block font-medium ml-1 dark:text-[#ffff]'>Your name</label>
-                                <input type="text" required {...register("sellerName")} id="" value={user && user.displayName} className='py-2 px-3 shadow-lg border-2 outline-none rounded-md w-full' />
+                                <label htmlFor="instructorName" className='block font-medium ml-1 dark:text-[#ffff]'>Your name</label>
+                                <input type="text" {...register("instructorName")} id="" value={user && user.displayName} className='py-2 px-3 shadow-lg dark:bg-slate-700 border-2 outline-none rounded-md w-full' />
+                                {errors.instructorName && <span>This field is required</span>}
                             </div>
                             <div className='space-y-1'>
-                                <label htmlFor="sellerEmail" className='block font-medium ml-1 dark:text-[#ffff]'>Your email</label>
-                                <input type="text" required {...register("sellerEmail")} id="" value={user && user.email} className='py-2 px-3 shadow-lg border-2 outline-none rounded-md w-full' />
+                                <label htmlFor="instructorEmail" className='block font-medium ml-1 dark:text-[#ffff]'>Your email</label>
+                                <input type="text" {...register("instructorEmail")} id="" value={user && user.email} className='py-2 px-3 shadow-lg dark:bg-slate-700 border-2 outline-none rounded-md w-full' />
+                                {errors.instructorEmail && <span>This field is required</span>}
                             </div>
                         </div>
                         <div className='grid lg:grid-cols-2 gap-3 items-center'>
                             <div className="space-y-1">
-                                <label htmlFor="price" className='block font-medium ml-1 dark:text-[#ffff]'>Enter the product price</label>
-                                <input type="text" required {...register("price")} id="" placeholder='Enter the price' className='py-2 px-3 shadow-lg border-2 outline-none rounded-md w-full' />
+                                <label htmlFor="availableSeats" className='block font-medium ml-1 dark:text-[#ffff]'>Available Seats</label>
+                                <input type="text" {...register("availableSeats", { required: true })} id="" placeholder='Enter your class capacity' className='py-2 px-3 shadow-lg dark:bg-slate-700 border-2 outline-none rounded-md w-full' />
+                                {errors.availableSeats && <span>This field is required</span>}
                             </div>
                             <div className="space-y-1">
-                                <label htmlFor="ratings" className='block font-medium ml-1 dark:text-[#ffff]'>Enter the product ratings</label>
-                                <input type="text" required {...register("ratings")} id="" placeholder='Enter the ratings' className='py-2 px-3 shadow-lg border-2 outline-none rounded-md w-full' />
+                                <label htmlFor="price" className='block font-medium ml-1 dark:text-[#ffff]'>Class Price</label>
+                                <input type="text" {...register("price", { required: true })} id="" placeholder='Enter the fee for your class' className='py-2 px-3 shadow-lg dark:bg-slate-700 border-2 outline-none rounded-md w-full' />
+                                {errors.price && <span>This field is required</span>}
                             </div>
-                        </div>
-                        <div className='grid lg:grid-cols-2 gap-3 items-center'>
-                            <div className="space-y-1">
-                                <label htmlFor="subCategory" className='block font-medium ml-1 dark:text-[#ffff]'>Enter the product category</label>
-                                <select required {...register("subCategory")} className="select shadow-lg border-2 outline-none rounded-md w-full">
-                                    <option >Select Category</option>
-                                    <option value={'Marvel'}>Marvel</option>
-                                    <option value={'Transformers'}>Transformers</option>
-                                    <option value={'StarWars'}>StarWars</option>
-                                </select>
-                            </div>
-                            <div className="space-y-1">
-                                <label htmlFor="quantity" className='block font-medium ml-1 dark:text-[#ffff]'>Enter the product quantity</label>
-                                <input type="text" required {...register("quantity")} id="" placeholder='Enter the quantity' className='py-2 px-3 shadow-lg border-2 outline-none rounded-md w-full' />
-                            </div>
-                        </div>
-                        <div className="space-y-1">
-                            <label htmlFor="details" className='block font-medium ml-1 dark:text-[#ffff]'>Enter product details</label>
-                            <textarea {...register("details")} required id="" cols="30" rows="8" placeholder='Product details...' className='py-2 px-3 shadow-lg border-2 outline-none rounded-md w-full resize-none'></textarea>
                         </div>
                     </div>
-
-                    {errors.exampleRequired && <span>This field is required</span>}
 
                     <input type="submit" value="Add Product" className='btn btn-error w-full text-white font-semibold tracking-widest mt-8 drop-shadow-xl' />
                 </form>
