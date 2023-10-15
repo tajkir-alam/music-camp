fb6c78cca202da29f69cb5af191ff48ffb5bb6cb
tajkir-alam <tajkir.alam@gmail.com>
2023-10-15 18:15:33 +0600

    classSchedule section added

--- src/Pages/Home/ClassSchedule/ClassSchedule.jsx
+++ src/Pages/Home/ClassSchedule/ClassSchedule.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SectionTitle from '../../../Components/SectionTitle';
-import { FaSun } from 'react-icons/fa';
+import { FaCloudMoon, FaCloudSun, FaSun } from 'react-icons/fa';
 
 const ClassSchedule = () => {
     return (
@@ -11,31 +11,37 @@ const ClassSchedule = () => {
             />
 
             <div className='custom-container grid lg:grid-cols-3 gap-4'>
-                <div className="card shadow-xl">
-                    <div className=''>
-                        <FaSun />
+                <div className="card bg-black/5 hover:shadow-lg py-5 space-y-5 duration-300">
+                    <div className='flex justify-center items-center'>
+                        <FaCloudSun className='text-2xl' />
                     </div>
-                    <div className='mt-4 mb-8'>
-                        <h2 className='text-center text-lg md:text-xl text-slate-700 dark:text-white font-semibold'>Hello</h2>
-                        <p className='text-center text-sm text-slate-600 dark:text-slate-300 font-semibold -mt-1'>Teaches 29 students</p>
+                    <div className='space-y-1'>
+                        <h2 className='text-center text-lg md:text-xl text-slate-700 dark:text-white font-semibold'>Morning</h2>
+                        <p className='text-center text-sm text-slate-500 dark:text-slate-300 font-semibold'>
+                            Time: 10AM ~ 12Pm
+                        </p>
                     </div>
                 </div>
-                <div className="card shadow-xl">
+                <div className="card bg-black/5 hover:shadow-lg py-5 space-y-5 duration-300">
                     <div className='flex justify-center items-center'>
-                        <FaSun />
+                        <FaSun className='text-2xl' />
                     </div>
-                    <div className='mt-4 mb-8'>
-                        <h2 className='text-center text-lg md:text-xl text-slate-700 dark:text-white font-semibold'>Hello</h2>
-                        <p className='text-center text-sm text-slate-600 dark:text-slate-300 font-semibold -mt-1'>Teaches 29 students</p>
+                    <div className='space-y-1'>
+                        <h2 className='text-center text-lg md:text-xl text-slate-700 dark:text-white font-semibold'>Afternoon</h2>
+                        <p className='text-center text-sm text-slate-500 dark:text-slate-300 font-semibold'>
+                        Time: 12PM ~ 2Pm
+                        </p>
                     </div>
                 </div>
-                <div className="card shadow-xl">
+                <div className="card bg-black/5 hover:shadow-lg py-5 space-y-5 duration-300">
                     <div className='flex justify-center items-center'>
-                        <FaSun />
+                        <FaCloudMoon className='text-2xl' />
                     </div>
-                    <div className='mt-4 mb-8'>
-                        <h2 className='text-center text-lg md:text-xl text-slate-700 dark:text-white font-semibold'>Hello</h2>
-                        <p className='text-center text-sm text-slate-600 dark:text-slate-300 font-semibold -mt-1'>Teaches 29 students</p>
+                    <div className='space-y-1'>
+                        <h2 className='text-center text-lg md:text-xl text-slate-700 dark:text-white font-semibold'>Evening</h2>
+                        <p className='text-center text-sm text-slate-500 dark:text-slate-300 font-semibold'>
+                        Time: 2PM ~ 4PM
+                        </p>
                     </div>
                 </div>
             </div>
