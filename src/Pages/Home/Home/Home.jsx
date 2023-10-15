9d06b23eddedf45ee5c03fb92f27e73c8f3cdc48
tajkir-alam <tajkir.alam@gmail.com>
2023-10-15 17:16:25 +0600

    join us section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,15 +3,17 @@ import Banner from '../Banner/Banner';
 import PopularClasses from '../PopularClasses/PopularClasses';
 import PopularInstructors from '../PopularInstructors/PopularInstructors';
 import Review from '../Review/Review';
+import JoinUs from '../JoinUs/JoinUs';
 
 const Home = () => {
     return (
-        <div className='mb-14'>
+        <>
             <Banner></Banner>
             <PopularClasses></PopularClasses>
             <PopularInstructors></PopularInstructors>
             <Review></Review>
-        </div>
+            <JoinUs></JoinUs>
+        </>
     );
 };
 
---
+++ src/Pages/Home/JoinUs/JoinUs.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const JoinUs = () => {
+    return (
+        <div className='bg-[#0c0b0bdc] text-white tracking-widest py-12 space-y-4'>
+            <h3 className='text-center text-3xl'>Join Our Class Now and Get a Chance To win Free Extra Class</h3>
+            <div className='text-center'>
+                <Link to='/registration' className='btn btn-error text-white tracking-wider hover:bg-error/90'>Join Now</Link>
+            </div>
+        </div>
+
+    );
+};
+
+export default JoinUs;
