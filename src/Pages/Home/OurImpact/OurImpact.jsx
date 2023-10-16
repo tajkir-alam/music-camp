346aa12b629e5772a8c94b7fb45aa4bc69d572b8
tajkir-alam <tajkir.alam@gmail.com>
2023-10-16 22:13:52 +0600

    our impact section done

--- src/Pages/Home/OurImpact/OurImpact.jsx
+++ src/Pages/Home/OurImpact/OurImpact.jsx
@@ -5,20 +5,27 @@ const OurImpact = () => {
     return (
         <section className='custom-container'>
             <SectionTitle
-                heading={'Our Students Review'}
-                subHeading={'We are happy to get the valuable thoughts from our customers. Thanks for being with us. Keep shopping action figures and play.'}
+                heading={'Our Impact'}
+                subHeading={'We are happy for our Assets'}
             />
             <div className='grid lg:grid-cols-3 justify-center items-center gap-4'>
-                <div className='bg-[#f1eaea8f] rounded-tl-3xl rounded-tr-lg rounded-br-3xl rounded-bl-lg px-4 py-8 h-64 grid grid-cols-3 items-center gap-4'>
-                    <div>
-                        {/* <img src={img2} alt="" className='h-full w-full opacity-75' /> */}
-                    </div>
-                    <div className='col-span-2 space-y-2'>
-                        <h2 className='text-2xl font-bold text-slate-600'>Hokwana Bakum</h2>
-                        <p className='text-lg font-light'>
-                            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, fugiat."
-                        </p>
-                    </div>
+                <div className='bg-[#f1eaea8f] rounded-tl-3xl rounded-tr-lg rounded-br-3xl rounded-bl-lg py-8 text-center space-y-1'>
+                    <h2 className='text-2xl font-bold text-slate-600'>Total Students</h2>
+                    <p className='text-lg text-blue-600/80 font-semibold tracking-widest'>
+                        1,200 +
+                    </p>
+                </div>
+                <div className='bg-[#f1eaea8f] rounded-tl-3xl rounded-tr-lg rounded-br-3xl rounded-bl-lg py-8 text-center space-y-1'>
+                    <h2 className='text-2xl font-bold text-slate-600'>Total Course</h2>
+                    <p className='text-lg text-blue-600/80 font-semibold tracking-widest'>
+                        45+
+                    </p>
+                </div>
+                <div className='bg-[#f1eaea8f] rounded-tl-3xl rounded-tr-lg rounded-br-3xl rounded-bl-lg py-8 text-center space-y-1'>
+                    <h2 className='text-2xl font-bold text-slate-600'>Total Instructors</h2>
+                    <p className='text-lg text-blue-600/80 font-semibold tracking-widest'>
+                        23+
+                    </p>
                 </div>
             </div>
         </section>
